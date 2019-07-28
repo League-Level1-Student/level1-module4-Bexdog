10cbf88340cf4ca000245a069f7779f8069f936e
league <league@imac-14.attlocal.net>
2019-07-28 11:28:10 -0700

    slot machine

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,4 +1,38 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SlotMachine {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+public static void main(String[] args) {
+	
+}
+void run() {
+	button.setText("SPIN!");
+	panel.add(button);
+	
+	frame.add(panel);
+	frame.setVisible(true);
+	frame.pack();
+}
+private JLabel createLabelImage(String fileName) throws MalformedURLException{
+    URL imageURL = getClass().getResource(fileName);
+if (imageURL == null){
+	System.err.println("Could not find image " + fileName);
+	return new JLabel();
+}
+Icon icon = new ImageIcon(imageURL);
+JLabel imageLabel = new JLabel(icon);
+return imageLabel;
+}
 
 }
+
